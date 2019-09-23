38c29d0a94a2b8df26689e1f2f07629dff569f25
Chase <chezchase3@gmail.com>
2019-09-23 09:47:37 -0600

    Added Edwin Print Statements

--- src/monster/model/Controller.java
+++ src/monster/model/Controller.java
@@ -13,7 +13,10 @@ public class Controller
 	}	
 	public void start()
 	{
-		System.out.println(defaultMonster);
 		System.out.println(deadlyMonsterOfDoom);
+		System.out.println(deadlyMonsterOfDoom.getName() + " is in posession of " + deadlyMonsterOfDoom.getAntennaAmount() + " Antennae. You should fear him");
+		System.out.println(deadlyMonsterOfDoom.getName() + " Has your address and also " + deadlyMonsterOfDoom.getAppendageCount() + " Limbs. With this information, he can take over your house.");
+		System.out.println(deadlyMonsterOfDoom.getName() + " makes 1.5 million dollars per year. With this money, he can afford glasses for each and every one of his " + deadlyMonsterOfDoom.getEyeCount() + " eyes.");
+		
 	}
 }
