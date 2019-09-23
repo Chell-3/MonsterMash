ec2e1e00e92a7cb55fe9b31430bef1d9db5ada2d
Chase <chezchase3@gmail.com>
2019-09-23 09:35:01 -0600

    Added getters to the object

--- src/monster/model/Controller.java
+++ src/monster/model/Controller.java
@@ -4,8 +4,16 @@ import monster.thing.MarshmallowMonster;
 
 public class Controller
 {
-	void start()
+	private MarshmallowMonster defaultMonster;
+	private MarshmallowMonster deadlyMonsterOfDoom;
+	public Controller()
 	{
-		MarshmallowMonster app = new MarshmallowMonster();
+		defaultMonster = new MarshmallowMonster();
+		deadlyMonsterOfDoom = new MarshmallowMonster(5,1,15,false,3,2,"Edwin");
+	}	
+	public void start()
+	{
+		System.out.println(defaultMonster);
+		System.out.println(deadlyMonsterOfDoom);
 	}
 }
--- src/monster/thing/MarshmallowMonster.java
+++ src/monster/thing/MarshmallowMonster.java
@@ -20,4 +20,49 @@ public class MarshmallowMonster
 		this.buttonCount = -99;
 		this.name = "Edwin, the kindhearted";
 	}
+	
+	public MarshmallowMonster(int appendageCount, double antennaAmount, int eyeCount, boolean hasMoles, int mouthCount, int buttonCount, String name)
+	{
+		this.appendageCount = appendageCount;
+		this.antennaAmount = antennaAmount;
+		this.eyeCount = eyeCount;
+		this.hasMoles = hasMoles;
+		this.mouthCount = mouthCount;
+		this.buttonCount = buttonCount;
+		this.name = name;
+	}
+	
+	public int getAppendageCount()
+	{
+		return appendageCount;
+	}
+	public double getAntennaAmount()
+	{
+		return antennaAmount;
+	}
+	public int getEyeCount()
+	{
+		return eyeCount;
+	}
+	public boolean getHasMoles()
+	{
+		return hasMoles;
+	}
+	public int getMouthCount()
+	{
+		return mouthCount;
+	}
+	public double getButtonCount()
+	{
+		return buttonCount;
+	}
+	public String getName()
+	{
+		return name;
+	}
+	public String toString()
+	{
+		String description = "sup bruh, i be da monstah known only as " + name;
+		return description;
+	}
 }
