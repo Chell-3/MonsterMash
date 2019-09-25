9684735e93c17861e0396d15e5cc0b3ddc371377
Chase <chezchase3@gmail.com>
2019-09-25 09:16:12 -0600

    Added the SCANNER OF DOOM

--- src/monster/model/Controller.java
+++ src/monster/model/Controller.java
@@ -1,23 +1,42 @@
 package monster.model;
 
+import java.util.Scanner;
+
 import monster.thing.MarshmallowMonster;
 
 public class Controller
 {
 	private MarshmallowMonster defaultMonster;
 	private MarshmallowMonster deadlyMonsterOfDoom;
+	private Scanner inputScanner;
 	public Controller()
 	{
+		inputScanner = new Scanner(System.in);
 		defaultMonster = new MarshmallowMonster();
 		deadlyMonsterOfDoom = new MarshmallowMonster(5,1,15,false,3,2,"Edwin");
 	}	
 	public void start()
 	{
+		System.out.println("Would you like to create a monster?");
+		String answer = inputScanner.nextLine();
+		if (answer.equalsIgnoreCase("yes"))
+		{
+			makeMonsters();
+		}
+		else
+		{
+			System.out.println("Ok then. Let me telly you about my monster, " + deadlyMonsterOfDoom.getName());
+		}
+		inputScanner.close();
 		System.out.println(deadlyMonsterOfDoom);
 		System.out.println(deadlyMonsterOfDoom.getName() + " is in posession of " + deadlyMonsterOfDoom.getAntennaAmount() + " Antennae. You should fear him");
 		System.out.println(deadlyMonsterOfDoom.getName() + " Has your address and also " + deadlyMonsterOfDoom.getAppendageCount() + " Limbs. With this information, he can take over your house.");
 		System.out.println(deadlyMonsterOfDoom.getName() + " makes 1.5 million dollars per year. With this money, he can afford glasses for each and every one of his " + deadlyMonsterOfDoom.getEyeCount() + " eyes.");
 		System.out.println("I certainly fear " + deadlyMonsterOfDoom.getName() + " and his " + deadlyMonsterOfDoom.getMouthCount() + " perfect mouths.");
 		System.out.println("Luckily, however, " + deadlyMonsterOfDoom.getName() + " has multiple buttons that can be pressed. to be specific, he has " + deadlyMonsterOfDoom.getButtonCount() + " buttons.");
+	}
+	private void makeMonsters()
+	{
+		
 	}
 }
