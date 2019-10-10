e6d55d1033549830a55720117f23fadd9493ed17
Chase <chezchase3@gmail.com>
2019-10-10 08:55:43 -0600

    changed EVERYTHING to popup window interaction.

--- src/monster/model/Controller.java
+++ src/monster/model/Controller.java
@@ -1,72 +1,117 @@
 package monster.model;
 
+import third.view.Popup;
 import java.util.Scanner;
 
 import monster.thing.MarshmallowMonster;
 
 public class Controller
 {
+	private Popup view;
 	private MarshmallowMonster defaultMonster;
 	private MarshmallowMonster deadlyMonsterOfDoom;
 	private Scanner inputScanner;
 	public Controller()
 	{
+		view = new Popup();
 		inputScanner = new Scanner(System.in);
 		defaultMonster = new MarshmallowMonster(5,1,15,false,3,2,"Edwin");
 		deadlyMonsterOfDoom = new MarshmallowMonster(5,1,15,false,3,2,"Edwin");
 	}	
 	public void start()
 	{
-		System.out.println("Would you like to create a monster?");
-		String answer = inputScanner.nextLine();
+		String answer = view.askQuestion("Would you like to create a monster?");
 		if (answer.equalsIgnoreCase("yes"))
 		{
-			makeManyMonsters();
+			makeMonsters();
 		}
 		else
 		{
-			System.out.println("Ok then. Let me telly you about my monster, " + deadlyMonsterOfDoom.getName());
+			view.displayMessage("Ok then. Let me telly you about my monster, " + deadlyMonsterOfDoom.getName());
 		}
 		inputScanner.close();
-		System.out.println(deadlyMonsterOfDoom);
-		System.out.println(deadlyMonsterOfDoom.getName() + " is in posession of " + deadlyMonsterOfDoom.getAntennaAmount() + " Antennae. You should fear him");
-		System.out.println(deadlyMonsterOfDoom.getName() + " Has your address and also " + deadlyMonsterOfDoom.getAppendageCount() + " Limbs. With this information, he can take over your house.");
-		System.out.println(deadlyMonsterOfDoom.getName() + " makes 1.5 million dollars per year. With this money, he can afford glasses for each and every one of his " + deadlyMonsterOfDoom.getEyeCount() + " eyes.");
-		System.out.println("I certainly fear " + deadlyMonsterOfDoom.getName() + " and his " + deadlyMonsterOfDoom.getMouthCount() + " perfect mouths.");
-		System.out.println("Luckily, however, " + deadlyMonsterOfDoom.getName() + " has multiple buttons that can be pressed. to be specific, he has " + deadlyMonsterOfDoom.getButtonCount() + " buttons.");
+		view.displayMessage(deadlyMonsterOfDoom.getName() + " is in posession of " + deadlyMonsterOfDoom.getAntennaAmount() + " Antennae. You should fear him.\n" +	deadlyMonsterOfDoom.getName() + " Has your address and also " + deadlyMonsterOfDoom.getAppendageCount() + " Limbs. With this information, he can take over your house.\n" + deadlyMonsterOfDoom.getName() + " makes 1.5 million dollars per year. With this money, he can afford glasses for each and every one of his " + deadlyMonsterOfDoom.getEyeCount() + " eyes.\nI certainly fear " + deadlyMonsterOfDoom.getName() + " and his " + deadlyMonsterOfDoom.getMouthCount() + " perfect mouths.\nLuckily, however, " + deadlyMonsterOfDoom.getName() + " has multiple buttons that can be pressed. to be specific, he has " + deadlyMonsterOfDoom.getButtonCount() + " buttons.");
 	}
 	private void makeMonsters()
 	{
 		String response = "";
-		System.out.println("What do you want your monster's name to be?");
-		response = inputScanner.nextLine();
+		response = view.askQuestion("What do you want the monster to be named?");
 		deadlyMonsterOfDoom.setName(response);
-		int wholeNumber = 0;
-		double realNumber = 0.0;
-		System.out.println("How many limbs does your monster have?");
-		wholeNumber = inputScanner.nextInt();
-		inputScanner.nextLine();
-		deadlyMonsterOfDoom.setAppendageCount(wholeNumber);
-		System.out.println("How many antennae does your monster have?");
-		realNumber = inputScanner.nextDouble();
-		inputScanner.nextLine();
-		deadlyMonsterOfDoom.setAntennaAmount(realNumber);
-		System.out.println("Does your monster have any moles?");
-		String boolInput = inputScanner.nextLine();
-		deadlyMonsterOfDoom.setHasMoles((boolInput.equalsIgnoreCase("yes")||boolInput.equalsIgnoreCase("Y")));
+		response = view.askQuestion("How many appendages for " + deadlyMonsterOfDoom.getName());
+		while(!validateInt(response))
+		{
+			response = view.askQuestion("That's not an acceptable number. don't type it out, and it has to be a whole number.");
+		}
+		deadlyMonsterOfDoom.setAppendageCount(Integer.parseInt(response));
+		
+		
+		response = view.askQuestion("How many antennae does your monster have?");
+		while(!validateDouble(response))
+		{
+			response = view.askQuestion("That's not an acceptable double(Type a decimal or something).");
+		}
+		deadlyMonsterOfDoom.setAntennaAmount(Double.parseDouble(response));
+		response = view.askQuestion("Does " + deadlyMonsterOfDoom.getName() + " have any moles?");
+		deadlyMonsterOfDoom.setHasMoles((response.equalsIgnoreCase("yes")||response.equalsIgnoreCase("Y")));
+		
+		response = view.askQuestion("How many eyes does " + deadlyMonsterOfDoom.getName() + " have?");
+		while(!validateInt(response))
+		{
+			response = view.askQuestion("That's not an acceptable number. don't type it out, and it has to be a whole number.");
+		}
+		deadlyMonsterOfDoom.setEyeCount(Integer.parseInt(response));
+		
+		response = view.askQuestion("How many mouths does " + deadlyMonsterOfDoom.getName() + " have?");
+		while(!validateInt(response))
+		{
+			response = view.askQuestion("That's not an acceptable number. don't type it out, and it has to be a whole number.");
+		}
+		deadlyMonsterOfDoom.setMouthCount(Integer.parseInt(response));
+		
+		response = view.askQuestion("How many buttons does " + deadlyMonsterOfDoom.getName() + " have?");
+		while(!validateInt(response))
+		{
+			response = view.askQuestion("That's not an acceptable number. don't type it out, and it has to be a whole number.");
+		}
+		deadlyMonsterOfDoom.setButtonCount(Integer.parseInt(response));
+		view.displayMessage("OK!\n\nIt's time to meet your monster!");
+		
 	}
 	private void makeManyMonsters()
 	{
 		MarshmallowMonster list[] = {deadlyMonsterOfDoom,deadlyMonsterOfDoom,deadlyMonsterOfDoom,deadlyMonsterOfDoom,deadlyMonsterOfDoom,deadlyMonsterOfDoom,deadlyMonsterOfDoom,deadlyMonsterOfDoom,deadlyMonsterOfDoom,deadlyMonsterOfDoom,deadlyMonsterOfDoom,deadlyMonsterOfDoom,deadlyMonsterOfDoom,deadlyMonsterOfDoom,deadlyMonsterOfDoom,deadlyMonsterOfDoom,deadlyMonsterOfDoom,deadlyMonsterOfDoom,deadlyMonsterOfDoom,deadlyMonsterOfDoom,deadlyMonsterOfDoom,deadlyMonsterOfDoom,deadlyMonsterOfDoom};
 		for(int monsterCount = 0; monsterCount < 4; monsterCount++)
 		{
-			MarshmallowMonster mine = new MarshmallowMonster();
-			list[monsterCount] = mine;
+			makeMonsters();
 		}
-		System.out.println(list[0]);
-		System.out.println(list[1]);
-		System.out.println(list[2]);
-		System.out.println(list[3]);
-		System.out.println(list[4]);
+	}
+	public boolean validateInt(String input)
+	{
+		boolean isValid = false;
+		
+		try
+		{
+			Integer.parseInt(input);
+			isValid = true;
+		}
+		catch (NumberFormatException error)
+		{
+			view.displayMessage("That is not an int. Try again!");
+		}
+		return isValid;
+	}
+	public boolean validateDouble(String input)
+	{
+		boolean isValid = false;
+		try
+		{
+			Double.parseDouble(input);
+			isValid = true;
+		}
+		catch(NumberFormatException error)
+		{
+			view.displayMessage("That is not a Double (Number with a decimal or something). Try again!");
+		}
+		return isValid;
 	}
 }
